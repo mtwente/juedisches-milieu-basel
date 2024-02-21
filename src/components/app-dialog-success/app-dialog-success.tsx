d7400a108cf2fb13b9efd18750f9b98f6bbb42a3
joschne <jonas.schneider@balcab.ch>
2024-02-21 15:16:25 +0100

    fix

--- src/components/app-dialog-success/app-dialog-success.tsx
+++ src/components/app-dialog-success/app-dialog-success.tsx
@@ -12,14 +12,14 @@ export class AppDialogSuccess {
   @Prop() nr: number;
   @Method() async open() {
     await this.successModal.present();
-    // const t = setTimeout(async () => {
-    //   await this.successModal.dismiss();
-    //   await this.dismiss();
-    // }, 2500);
-    // this.successModal.onDidDismiss().then(() => {
-    //   clearTimeout(t);
-    //   this.dismiss();
-    // });
+    const t = setTimeout(async () => {
+      await this.successModal.dismiss();
+      await this.dismiss();
+    }, 2500);
+    this.successModal.onDidDismiss().then(() => {
+      clearTimeout(t);
+      this.dismiss();
+    });
   }
 
   render() {
